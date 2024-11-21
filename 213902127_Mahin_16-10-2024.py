9af86789f89c38252fce8470a8701210e47059b3
Md.Jubayerul Hasan Mahin <134420032+Maahin11@users.noreply.github.com>
2024-11-21 10:30:22 +0600

    Add files via upload

---
+++ 213902127_Mahin_16-10-2024.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+print("How are you Mr. "+name+".Have a good day")
+
+age = input("Enter your age: ")
+
+name = input("Enter Your name: ")
+print("Your age is: ")
+print("How are Mr. "+name+"Have a good day")
+
+a = int(input("Put a number:"))
+b = int(input("Put a number:"))
+s  = a+b
+print("Summation is: "+str(s))
+print("Summation is: ", s)
+print(s)
